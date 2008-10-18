43b88bb6411a2f404c64b6ee07e0d6bb5fa359b9
julien <julien@c210c87e-2b05-0410-b4a6-82fbe8e97a6a>
2008-10-18 07:34:40 +0000

    - adding DataBuffer, File and Movie to the C API - adding explicit constants for AP4_Track::Type enum - removing GetMetaData from AP4_Movie (was not implemented and redundant with AP4_File::GetMetaData) - returning AP4_Duration as opposed AP4_UI32 for AP4_Movie::GetDurationMs

--- trunk/Source/C++/CApi/Bento4.cpp
+++ trunk/Source/C++/CApi/Bento4.cpp
@@ -35,12 +35,22 @@
 /*----------------------------------------------------------------------
 |   constants
 +---------------------------------------------------------------------*/
-const int AP4_FILE_BYTE_STREAM_MODE_READ = AP4_FileByteStream::STREAM_MODE_READ;
-const int AP4_FILE_BYTE_STREAM_MODE_WRITE = AP4_FileByteStream::STREAM_MODE_WRITE;
+const int AP4_FILE_BYTE_STREAM_MODE_READ       = AP4_FileByteStream::STREAM_MODE_READ;
+const int AP4_FILE_BYTE_STREAM_MODE_WRITE      = AP4_FileByteStream::STREAM_MODE_WRITE;
 const int AP4_FILE_BYTE_STREAM_MODE_READ_WRITE = AP4_FileByteStream::STREAM_MODE_READ_WRITE;
 
+const int AP4_TRACK_TYPE_UNKNOWN = AP4_Track::TYPE_UNKNOWN;
+const int AP4_TRACK_TYPE_AUDIO   = AP4_Track::TYPE_AUDIO;
+const int AP4_TRACK_TYPE_VIDEO   = AP4_Track::TYPE_VIDEO;
+const int AP4_TRACK_TYPE_SYSTEM  = AP4_Track::TYPE_SYSTEM;
+const int AP4_TRACK_TYPE_HINT    = AP4_Track::TYPE_HINT;
+const int AP4_TRACK_TYPE_TEXT    = AP4_Track::TYPE_TEXT;
+const int AP4_TRACK_TYPE_JPEG    = AP4_Track::TYPE_JPEG;
+const int AP4_TRACK_TYPE_RTP     = AP4_Track::TYPE_RTP;
+
+
 /*----------------------------------------------------------------------
-|   AP4_ByteStream_AddReference
+|   AP4_ByteStream implementation
 +---------------------------------------------------------------------*/
 void 
 AP4_ByteStream_AddReference(AP4_ByteStream* self)
@@ -48,18 +58,12 @@ AP4_ByteStream_AddReference(AP4_ByteStream* self)
     self->AddReference();
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_Release
-+---------------------------------------------------------------------*/
 void 
 AP4_ByteStream_Release(AP4_ByteStream* self)
 {
     self->Release();
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_ReadPartial
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_ReadPartial(AP4_ByteStream*  self,
                            void*            buffer,
@@ -69,9 +73,6 @@ AP4_ByteStream_ReadPartial(AP4_ByteStream*  self,
     return self->ReadPartial(buffer, bytes_to_read, *bytes_read);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_Read
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_Read(AP4_ByteStream* self, 
                     void*           buffer, 
@@ -80,63 +81,42 @@ AP4_ByteStream_Read(AP4_ByteStream* self,
     return self->Read(buffer, bytes_to_read);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_ReadDouble
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_ReadDouble(AP4_ByteStream* self, double* value)
 {
     return self->ReadDouble(*value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_ReadUI64
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_ReadUI64(AP4_ByteStream* self, AP4_UI64* value)
 {
     return self->ReadUI64(*value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_ReadUI32
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_ReadUI32(AP4_ByteStream* self, AP4_UI32* value)
 {
     return self->ReadUI32(*value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_ReadUI24
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_ReadUI24(AP4_ByteStream* self, AP4_UI32* value)
 {
     return self->ReadUI24(*value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_ReadUI16
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_ReadUI16(AP4_ByteStream* self, AP4_UI16* value)
 {
     return self->ReadUI16(*value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_ReadUI08
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_ReadUI08(AP4_ByteStream* self, AP4_UI08* value)
 {
     return self->ReadUI08(*value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_ReadString
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_ReadString(AP4_ByteStream* self, 
                           char*           buffer, 
@@ -145,9 +125,6 @@ AP4_ByteStream_ReadString(AP4_ByteStream* self,
     return self->ReadString(buffer, size);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_WritePartial
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_WritePartial(AP4_ByteStream* self,
                             const void*     buffer,
@@ -157,9 +134,6 @@ AP4_ByteStream_WritePartial(AP4_ByteStream* self,
     return self->WritePartial(buffer, bytes_to_write, *bytes_written);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_Write
-+---------------------------------------------------------------------*/
 AP4_Result
 AP4_ByteStream_Write(AP4_ByteStream* self,
                      const void*     buffer,
@@ -168,99 +142,66 @@ AP4_ByteStream_Write(AP4_ByteStream* self,
     return self->Write(buffer, bytes_to_write);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_WriteDouble
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_WriteDouble(AP4_ByteStream* self, double value)
 {
     return self->WriteDouble(value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_WriteUI64
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_WriteUI64(AP4_ByteStream* self, AP4_UI64 value)
 {
     return self->WriteUI64(value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_WriteUI32
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_WriteUI32(AP4_ByteStream* self, AP4_UI32 value)
 {
     return self->WriteUI32(value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_WriteUI24
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_WriteUI24(AP4_ByteStream* self, AP4_UI32 value)
 {
     return self->WriteUI24(value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_WriteUI16
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_WriteUI16(AP4_ByteStream* self, AP4_UI16 value)
 {
     return self->WriteUI16(value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_WriteUI08
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_WriteUI08(AP4_ByteStream* self, AP4_UI08 value)
 {
     return self->WriteUI08(value);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_WriteString
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_WriteString(AP4_ByteStream* self, const char* buffer)
 {
     return self->WriteString(buffer);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_Seek
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_Seek(AP4_ByteStream* self, AP4_Position position)
 {
     return self->Seek(position);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_Tell
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_Tell(AP4_ByteStream* self, AP4_Position* position)
 {
     return self->Tell(*position);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_GetSize
-+---------------------------------------------------------------------*/
 AP4_Result
 AP4_ByteStream_GetSize(AP4_ByteStream* self, AP4_LargeSize* size)
 {
     return self->GetSize(*size);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_ByteStream_CopyTo
-+---------------------------------------------------------------------*/
 AP4_Result 
 AP4_ByteStream_CopyTo(AP4_ByteStream* self, 
                       AP4_ByteStream* receiver, 
@@ -269,18 +210,12 @@ AP4_ByteStream_CopyTo(AP4_ByteStream* self,
     return self->CopyTo(*receiver, size);
 }
 
-/*----------------------------------------------------------------------
-|   Ap4_ByteStream_Flush
-+---------------------------------------------------------------------*/
 AP4_Result 
 Ap4_ByteStream_Flush(AP4_ByteStream* self)
 {
     return self->Flush();
 }
 
-/*----------------------------------------------------------------------
-|   AP4_SubStream_Create
-+---------------------------------------------------------------------*/
 AP4_ByteStream* 
 AP4_SubStream_Create(AP4_ByteStream* container, 
                      AP4_Position     position, 
@@ -289,41 +224,210 @@ AP4_SubStream_Create(AP4_ByteStream* container,
     return new AP4_SubStream(*container, position, size);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_MemoryByteStream_Create
-+---------------------------------------------------------------------*/
 AP4_ByteStream* 
 AP4_MemoryByteStream_Create(AP4_Size size)
 {
     return new AP4_MemoryByteStream(size);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_MemoryByteStream_FromBuffer
-+---------------------------------------------------------------------*/
 AP4_ByteStream* 
 AP4_MemoryByteStream_FromBuffer(const AP4_UI08* buffer, AP4_Size size)
 {
     return new AP4_MemoryByteStream(buffer, size);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_MemoryByteStream_AdaptDataBuffer
-+---------------------------------------------------------------------*/
 AP4_ByteStream*
 AP4_MemoryByteStream_AdaptDataBuffer(AP4_DataBuffer* buffer)
 {
     return new AP4_MemoryByteStream(*buffer);
 }
 
-/*----------------------------------------------------------------------
-|   AP4_FileByteStream_Create
-+---------------------------------------------------------------------*/
 AP4_ByteStream*
 AP4_FileByteStream_Create(const char* name, int mode)
 {
     return new AP4_FileByteStream(name, (AP4_FileByteStream::Mode) mode);
 }
 
+/*----------------------------------------------------------------------
+|   AP4_DataBuffer implementation
++---------------------------------------------------------------------*/
+AP4_Result 
+AP4_DataBuffer_SetBuffer(AP4_DataBuffer* self, AP4_Byte* buffer, AP4_Size size)
+{
+    return self->SetBuffer(buffer, size);
+}
 
+AP4_Result 
+AP4_DataBuffer_SetBufferSize(AP4_DataBuffer* self, AP4_Size size)
+{
+    return self->SetBufferSize(size);
+}
+
+AP4_Size 
+AP4_DataBuffer_GetBufferSize(const AP4_DataBuffer* self)
+{
+    return self->GetBufferSize();
+}
+
+const AP4_Byte*
+AP4_DataBuffer_GetData(const AP4_DataBuffer* self)
+{
+    return self->GetData();
+}
+
+AP4_Byte*
+AP4_DataBuffer_UseData(AP4_DataBuffer* self)
+{
+    return self->UseData();
+}
+
+AP4_Size
+AP4_DataBuffer_GetDataSize(const AP4_DataBuffer* self)
+{
+    return self->GetDataSize();
+}
+
+AP4_Result
+AP4_DataBuffer_SetDataSize(AP4_DataBuffer* self, AP4_Size size)
+{
+    return self->SetDataSize(size);
+}
+
+AP4_Result
+AP4_DataBuffer_SetData(AP4_DataBuffer* self, 
+                      const AP4_Byte*  data, 
+                      AP4_Size         data_size)
+{
+    return self->SetData(data, data_size);
+}
+                      
+AP4_Result
+AP4_DataBuffer_Reserve(AP4_DataBuffer* self, AP4_Size size)
+{
+    return self->Reserve(size);
+}
+
+AP4_DataBuffer*
+AP4_DataBuffer_Create(AP4_Size size)
+{
+    return new AP4_DataBuffer(size);
+}
+
+AP4_DataBuffer*
+AP4_DataBuffer_FromData(const void* data, AP4_Size data_size)
+{
+    return new AP4_DataBuffer(data, data_size);
+}
+
+AP4_DataBuffer*
+AP4_DataBuffer_Clone(const AP4_DataBuffer* other)
+{
+    return new AP4_DataBuffer(*other);
+}
+
+/*----------------------------------------------------------------------
+|   AP4_File implementation
++---------------------------------------------------------------------*/
+AP4_Movie*
+AP4_File_GetMovie(AP4_File* self)
+{
+    return self->GetMovie();
+}
+
+/* TODO AP4_File_GetFileType */
+
+AP4_Result 
+AP4_File_SetFileType(AP4_File*    self,
+                     AP4_UI32     major_brand,
+                     AP4_UI32     minor_version,
+                     AP4_UI32*    compatible_brands,
+                     AP4_Cardinal compatible_brand_count)
+{
+    return self->SetFileType(major_brand, 
+                             minor_version,
+                             compatible_brands,
+                             compatible_brand_count);
+}
+                     
+int
+AP4_File_IsMoovBeforeMdat(const AP4_File* self)
+{
+    return (int) self->IsMoovBeforeMdat();
+}
+
+/* TODO AP4_File_Inspect */
+
+const AP4_MetaData*
+AP4_File_GetMetaData(AP4_File* self)
+{
+    return self->GetMetaData();
+}
+
+AP4_File*
+AP4_File_Create(AP4_Movie* movie)
+{
+    return new AP4_File(movie);
+}
+
+AP4_File*
+AP4_File_FromStream(AP4_ByteStream* stream, int moov_only)
+{
+    return new AP4_File(*stream, AP4_DefaultAtomFactory::Instance, moov_only); 
+}
+
+/*----------------------------------------------------------------------
+|   AP4_Movie implementation
++---------------------------------------------------------------------*/
+AP4_Cardinal
+AP4_Movie_GetTrackCount(AP4_Movie* self)
+{
+    return self->GetTracks().ItemCount();
+}
+
+AP4_Track*
+AP4_Movie_GetTrackByIndex(AP4_Movie* self, AP4_Ordinal index)
+{
+    AP4_Track* track = NULL;
+    if (AP4_FAILED(self->GetTracks().Get(index, track))) {
+        return NULL;
+    } else {
+        return track;
+    }
+}
+
+AP4_Track*
+AP4_Movie_GetTrackById(AP4_Movie* self, AP4_UI32 track_id)
+{
+    return self->GetTrack(track_id);
+}
+
+AP4_Track*
+AP4_Movie_GetTrackByType(AP4_Movie* self, int type, AP4_Ordinal index)
+{
+    return self->GetTrack((AP4_Track::Type) type, index);
+}
+
+AP4_UI32
+AP4_Movie_GetTimeScale(AP4_Movie* self)
+{
+    return self->GetTimeScale();
+}
+
+AP4_UI32
+AP4_Movie_GetDuration(AP4_Movie* self)
+{
+    return self->GetDuration();
+}
+
+AP4_Duration
+AP4_Movie_GetDurationMs(AP4_Movie* self)
+{
+    return self->GetDurationMs();
+}
+
+AP4_Movie*
+AP4_Movie_Create(AP4_UI32 time_scale)
+{
+    return new AP4_Movie(time_scale);
+}
 
