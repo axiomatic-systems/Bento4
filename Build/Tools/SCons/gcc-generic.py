f78d1f8335ff0e7f6a89f6ca72f7f4c04c62aa7b
bok <bok@c210c87e-2b05-0410-b4a6-82fbe8e97a6a>
2009-02-22 03:37:21 +0000

    Better scons options

--- Build/Tools/SCons/gcc-generic.py
+++ Build/Tools/SCons/gcc-generic.py
@@ -1,37 +1,30 @@
-import os
+def generate(env, gcc_cross_prefix=None, gcc_strict=True, gcc_stop_on_warning=None):
+    if gcc_stop_on_warning == None: gcc_stop_on_warning = env['stop_on_warning']
 
-def generate(env, gcc_cross_prefix=None, gcc_extra_options='', gcc_relaxed_warnings=False):
-    ### general compiler flags
-    if gcc_relaxed_warnings:
-	c_compiler_compliance_flags = ''
-        cxx_compiler_warnings       = '-Wall'
+    ### compiler flags
+    if gcc_strict:
+        env.AppendUnique(CCFLAGS = ['-pedantic', '-Wall',  '-W',  '-Wundef', '-Wno-long-long'])
+        env.AppendUnique(CFLAGS  = ['-Wmissing-prototypes', '-Wmissing-declarations'])
     else:
-	c_compiler_compliance_flags = '-pedantic'
-        cxx_compiler_warnings       = '-Werror -Wall -W -Wundef -Wno-long-long'
-
-    c_compiler_warnings         = cxx_compiler_warnings + ' -Wmissing-prototypes -Wmissing-declarations'
-    c_compiler_defines          = '-D_REENTRANT'
-
-    if env['build_config'] == 'Debug':
-        c_compiler_flags = '-g'    
-    else:
-        c_compiler_flags = '-O3'
+        env.AppendUnique(CCFLAGS = ['-Wall'])
     
-    if env['enable_profiling']:
-        c_compiler_profile_flags = '-pg'
+    compiler_defines = ['-D_REENTRANT']
+    env.AppendUnique(CCFLAGS  = compiler_defines)
+    env.AppendUnique(CPPFLAGS = compiler_defines)
+    
+    if env['build_config'] == 'Debug':
+        env.AppendUnique(CCFLAGS = '-g')
     else:
-        c_compiler_profile_flags = ''
-                
+        env.AppendUnique(CCFLAGS = '-O3')
+    
+    if gcc_stop_on_warning:
+        env.AppendUnique(CCFLAGS = ['-Werror'])
+        
     if gcc_cross_prefix:
         env['ENV']['PATH'] += os.environ['PATH']
         env['AR']     = gcc_cross_prefix+'-ar'
         env['RANLIB'] = gcc_cross_prefix+'-ranlib'
-        env['CC']     = gcc_cross_prefix+'-gcc ' + gcc_extra_options
-        env['CXX']    = gcc_cross_prefix+'-g++ ' + gcc_extra_options
-        env['LINK']   = gcc_cross_prefix+'-g++ ' + gcc_extra_options
+        env['CC']     = gcc_cross_prefix+'-gcc'
+        env['CXX']    = gcc_cross_prefix+'-g++'
+        env['LINK']   = gcc_cross_prefix+'-g++'
 
-    ### general environment
-    env['CPPFLAGS']   = ' '.join([c_compiler_defines])
-    env['CFLAGS']     = ' '.join([c_compiler_profile_flags, c_compiler_compliance_flags, c_compiler_flags, c_compiler_warnings])
-    env['CXXFLAGS']   = ' '.join([c_compiler_profile_flags, c_compiler_compliance_flags, c_compiler_flags, cxx_compiler_warnings])
-    env['LINKFLAGS']  = c_compiler_profile_flags    
