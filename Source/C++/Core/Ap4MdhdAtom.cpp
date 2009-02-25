226c0a44f84f33faa3f5561699e47dcae42a743d
bok <bok@c210c87e-2b05-0410-b4a6-82fbe8e97a6a>
2009-02-25 06:03:55 +0000

    Fixed incorrect checking for version==0 in AP4_MdhdAtom's constructor

--- Source/C++/Core/Ap4MdhdAtom.cpp
+++ Source/C++/Core/Ap4MdhdAtom.cpp
@@ -42,7 +42,7 @@ AP4_MdhdAtom::Create(AP4_Size size, AP4_ByteStream& stream)
     AP4_UI32 version;
     AP4_UI32 flags;
     if (AP4_FAILED(AP4_Atom::ReadFullHeader(stream, version, flags))) return NULL;
-    if (version != 0) return NULL;
+    if (version > 1) return NULL;
     return new AP4_MdhdAtom(size, version, flags, stream);
 }
 
