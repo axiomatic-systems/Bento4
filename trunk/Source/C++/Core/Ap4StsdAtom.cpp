b2c3b5b78ba92ea2669c1af58748bb8853349237
bok <bok@c210c87e-2b05-0410-b4a6-82fbe8e97a6a>
2012-01-30 15:28:47 +0000

    Added support for stsd version 1

--- trunk/Source/C++/Core/Ap4StsdAtom.cpp
+++ trunk/Source/C++/Core/Ap4StsdAtom.cpp
@@ -52,7 +52,7 @@ AP4_StsdAtom::Create(AP4_Size         size,
     AP4_UI32 version;
     AP4_UI32 flags;
     if (AP4_FAILED(AP4_Atom::ReadFullHeader(stream, version, flags))) return NULL;
-    if (version != 0) return NULL;
+    if (version > 1) return NULL;
     return new AP4_StsdAtom(size, version, flags, stream, atom_factory);
 }
 
