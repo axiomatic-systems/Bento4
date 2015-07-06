938e2780491675029995451d6f78abc000f22603
Gilles Boccon-Gibod <bok@bok.net>
2015-07-05 21:47:57 -0700

    updated version number

--- Source/C++/Core/Ap4Version.h
+++ Source/C++/Core/Ap4Version.h
@@ -37,7 +37,7 @@
 /**
  * Version number of the SDK
  */
-#define AP4_VERSION        0x01040200
-#define AP4_VERSION_STRING "1.4.2.0"
+#define AP4_VERSION        0x01040300
+#define AP4_VERSION_STRING "1.4.3.0"
 
 #endif // _AP4_VERSION_H_
