f0f9d58e3f0a956f1691fef7ddc3d3b4dc839237
Gilles Boccon-Gibod <bok@bok.net>
2016-04-10 17:21:42 -0700

    prep for r609

--- Source/Python/utils/mp4-dash.py
+++ Source/Python/utils/mp4-dash.py
@@ -27,7 +27,7 @@ from subtitles import *
 
 # setup main options
 VERSION = "1.7.0"
-SDK_REVISION = '608'
+SDK_REVISION = '609'
 SCRIPT_PATH = path.abspath(path.dirname(__file__))
 sys.path += [SCRIPT_PATH]
 
