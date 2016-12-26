246f91b655d600c7a389e46b3fe0713a8009b76d
Seth Madison <seth@philo.com>
2016-12-26 17:36:23 -0500

    Fix a small memory leak in Ap4CommonEncryption

--- Source/C++/Core/Ap4CommonEncryption.cpp
+++ Source/C++/Core/Ap4CommonEncryption.cpp
@@ -1873,6 +1873,8 @@ public:
     m_SaizAtom(saiz_atom),
     m_SampleEncryptionAtom(sample_encryption_atom) {}
 
+    ~AP4_CencFragmentDecrypter() { delete m_SampleDecrypter; }
+
     // methods
     virtual AP4_Result ProcessFragment();
     virtual AP4_Result FinishFragment();
