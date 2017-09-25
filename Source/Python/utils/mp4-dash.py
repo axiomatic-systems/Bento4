3529ec7fb21b09d5e8be3d732adddd7dd9d25c69
Gilles Boccon-Gibod <bok@bok.net>
2017-09-24 20:16:38 -0700

    prep for r619

--- Source/Python/utils/mp4-dash.py
+++ Source/Python/utils/mp4-dash.py
@@ -28,7 +28,7 @@ from subtitles import *
 
 # setup main options
 VERSION = "1.8.0"
-SDK_REVISION = '618'
+SDK_REVISION = '619'
 SCRIPT_PATH = path.abspath(path.dirname(__file__))
 sys.path += [SCRIPT_PATH]
 
