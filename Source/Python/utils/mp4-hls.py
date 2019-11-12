6f91af5d55ce411c339132ead6bee80571af980f
Gilles Boccon-Gibod <bok@bok.net>
2019-11-11 21:18:01 -0800

    prep for r629

--- Source/Python/utils/mp4-hls.py
+++ Source/Python/utils/mp4-hls.py
@@ -22,7 +22,7 @@ from subtitles import *
 
 # setup main options
 VERSION = "1.1.0"
-SDK_REVISION = '628'
+SDK_REVISION = '629'
 SCRIPT_PATH = path.abspath(path.dirname(__file__))
 sys.path += [SCRIPT_PATH]
 
