330a29116588454c00edb3e2c791fece085ab0d6
Gilles Boccon-Gibod <bok@bok.net>
2020-04-26 18:24:27 -0700

    prep for r633

--- Source/Python/utils/mp4-dash.py
+++ Source/Python/utils/mp4-dash.py
@@ -57,7 +57,7 @@ from mp4utils import MakePsshBox,\
 
 # setup main options
 VERSION = "2.0.0"
-SDK_REVISION = '631'
+SDK_REVISION = '633'
 SCRIPT_PATH = path.abspath(path.dirname(__file__))
 sys.path += [SCRIPT_PATH]
 
