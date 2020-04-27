ce5333b2a521229e0735ec9de2ffe477520b3601
Gilles Boccon-Gibod <bok@bok.net>
2020-04-26 17:22:50 -0700

    wip

--- tasks/build.py
+++ tasks/build.py
@@ -2,17 +2,22 @@ import sys
 import os
 from invoke import task
 
-@task(default = True)
-def build(ctx):
+@task(default = True, help = { 'clean': 'Clean before building'})
+def build(ctx, clean = False):
+    if clean:
+        ctx.run("rm -rf cmakebuild")
     try:
         os.makedirs("cmakebuild")
     except:
         pass
     with ctx.cd("cmakebuild"):
-        ctx.run("rm CMakeCache.txt")
         generator = ''
         if sys.platform == 'darwin':
             generator = '-GXcode'
         command = "cmake {} -DCMAKE_BUILD_TYPE=Release ..".format(generator)
         ctx.run(command)
         ctx.run("cmake --build . --config Release")
+
+@task
+def sdk(ctx):
+    ctx.run("python3 Scripts/SdkPackager.py")
